52c8734dfde6e2e3ddbff077cf4e0558770d2968
cyq3721 <44914556+cyq3721@users.noreply.github.com>
2024-11-20 09:17:30 +0800

    emacs

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(spacemacs-dark))
+ '(package-selected-packages
+   '(emmet-mode web-mode counsel diff-hl diredfl org-pomodoro writeroom-mode org-superstar org-cliplink grab-mac-link ibuffer-vc keycast marginalia embark-consult consult embark vertico which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay rainbow-delimiters mode-line-bell vlf list-unicode-display unfill corfu-terminal corfu orderless disable-mouse default-text-scale ns-auto-titlebar dimmer spacemacs-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized command-log-mode scratch diminish gcmh exec-path-from-shell gnu-elpa-keyring-update seq))
+ '(spacemacs-theme-custom-colors
+   '((act1 . "##D48B1C")
+     (act2 . "#0000ff")
+     (base . "#ffffff"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ org-clock-save.el
@@ -0,0 +1,1 @@
+;; org-clock-save.el - tianshanpiaoxuedeMacBook-Pro.local at <2024-11-20 Wed 09:11>
