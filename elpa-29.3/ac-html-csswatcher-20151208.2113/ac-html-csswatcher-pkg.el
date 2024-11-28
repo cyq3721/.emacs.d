ccbd37287dfe63c3f1753f59aed9a7090141c766
cyq3721 <cyq3721@hotmail.com>
2024-11-28 09:45:01 +0800

    emacs

---
+++ elpa-29.3/ac-html-csswatcher-20151208.2113/ac-html-csswatcher-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ac-html-csswatcher" "20151208.2113"
+  "Css/less class/id completion with `ac-html' or `company-web'."
+  '((web-completion-data "0.1"))
+  :url "https://github.com/osv/ac-html-csswatcher"
+  :commit "b0f3e7e1a3fe49e88b6eb6432377232fc715f221"
+  :revdesc "b0f3e7e1a3fe"
+  :keywords '("html" "css" "less" "auto-complete")
+  :authors '(("Olexandr Sydorchuck" . "olexandr.syd@gmail.com"))
+  :maintainers '(("Olexandr Sydorchuck" . "olexandr.syd@gmail.com")))
