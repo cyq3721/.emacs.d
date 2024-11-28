4553177927ec2bac402ec8167c2cc8bc63bcb1f2
cyq3721 <cyq3721@hotmail.com>
2024-11-28 09:42:28 +0800

    emacs

--- custom.el
+++ custom.el
@@ -3,21 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
  '(custom-enabled-themes '(spacemacs-dark))
  '(package-selected-packages
-   '(company php-mode emmet-mode web-mode counsel diff-hl diredfl org-pomodoro writeroom-mode org-cliplink grab-mac-link org-superstar ibuffer-vc embark-consult consult embark vertico which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay rainbow-delimiters mode-line-bell vlf list-unicode-display unfill disable-mouse default-text-scale ns-auto-titlebar dimmer spacemacs-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized command-log-mode scratch diminish gcmh exec-path-from-shell gnu-elpa-keyring-update seq))
+   '(company-statistics ac-html-bootstrap ac-html-csswatcher company-web yasnippet company php-mode emmet-mode web-mode diff-hl diredfl org-pomodoro writeroom-mode org-cliplink grab-mac-link ox-pandoc org-superstar ibuffer-vc embark-consult consult embark vertico which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay rainbow-delimiters mode-line-bell vlf list-unicode-display unfill disable-mouse default-text-scale ns-auto-titlebar dimmer spacemacs-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized counsel command-log-mode scratch diminish gcmh exec-path-from-shell gnu-elpa-keyring-update seq))
  '(spacemacs-theme-custom-colors
    '((act1 . "##D48B1C")
      (act2 . "#0000ff")
-     (base . "#ffffff")))
- '(tool-bar-mode nil))
+     (base . "#ffffff"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Xingkai SC" :foundry "nil" :slant italic :weight light :height 120 :width normal))))
  '(company-preview ((t (:foreground "darkgray" :underline t))))
  '(company-preview-common ((t (:inherit company-preview))))
  '(company-tooltip ((t (:background "lightgray" :foreground "black"))))
