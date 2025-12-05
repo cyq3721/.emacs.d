302df302301a97a139edca498bf6016df3276860
cyq <cyq3721@gmail.com>
2025-12-05 13:01:02 +0800

    emacs

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -45,12 +45,51 @@
 
 ;; Web Beautify
 (use-package web-beautify
-  :hook ( web-mode . web-beautify)
-  :bind
-  ("C-c b h" . web-beautify-html)
-  ("C-c b c" . web-beautify-css)
-  ("C-c b j" . web-beautify-js)
-  ("C-c b s" . web-beautify-html))
+  :ensure t
+  :after (web-mode js2-mode)
+  
+  :init
+  ;; 更详细的配置
+  (setq web-beautify-html-indent-size 2
+        web-beautify-css-indent-size 2
+        web-beautify-js-indent-size 2
+        ;; HTML 特定设置
+        web-beautify-html-end-with-newline t
+        web-beautify-html-max-preserve-newlines 2
+        web-beautify-html-indent-inner-html t
+        web-beautify-html-wrap-line-length 120
+        ;; CSS 特定设置
+        web-beautify-css-end-with-newline t
+        web-beautify-css-selector-separator-newline t
+        web-beautify-css-newline-between-rules t)
+  
+  :config
+  ;; 自动检测文件类型并调用相应格式化函数
+  (defun my-web-beautify-buffer-smart ()
+    "智能格式化当前缓冲区"
+    (interactive)
+    (cond
+     ((derived-mode-p 'web-mode)
+      (let ((content-type (web-mode-content-type)))
+        (cond
+         ((string= content-type "html") (web-beautify-html-buffer))
+         ((string= content-type "css") (web-beautify-css-buffer))
+         ((string= content-type "javascript") (web-beautify-js-buffer))
+         (t (web-beautify-html-buffer)))))
+     ((derived-mode-p 'html-mode) (web-beautify-html-buffer))
+     ((derived-mode-p 'css-mode) (web-beautify-css-buffer))
+     ((derived-mode-p 'js-mode 'js2-mode 'javascript-mode) 
+      (web-beautify-js-buffer))
+     (t (message "当前模式不支持自动格式化"))))
+  
+  ;; 设置保存时自动格式化（可选）
+  (defun my-web-beautify-before-save ()
+    "保存前自动格式化"
+    (when (and (boundp 'web-beautify-auto-format-on-save)
+               web-beautify-auto-format-on-save)
+      (my-web-beautify-buffer-smart)))
+  
+  (add-hook 'before-save-hook 'my-web-beautify-before-save))
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
